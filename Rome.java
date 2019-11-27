ec77aa9d435cd00ad69a4fc89fefe6909c206193
Rushit-Jain <53978226+Rushit-Jain@users.noreply.github.com>
2019-11-27 19:39:36 +0530

    Add files via upload

--- Rome.java
+++ Rome.java
@@ -13,10 +13,17 @@ public class Rome
             }
             if(d>=5)
             {
-                rd=rd+"V";
-                d-=5;
-                for(i=1;i<=d;i++)
-                    rd=rd+"I";
+                if(d!=9)
+                {
+                    rd=rd+"V";
+                    d-=5;
+                    for(i=1;i<=d;i++)
+                        rd=rd+"I";
+                }
+                else
+                {
+                    rd=rd+romanDateMonth(Integer.toString(10-d))+romanDateMonth("10");
+                }
             }
         }
         else
@@ -33,30 +40,55 @@ public class Rome
     {
         String ry=""; int i; int d=Integer.parseInt(s);
         if(d<50)
-            ry=ry+romanDateMonth(s);
+        {
+            if(d<40)
+            {
+                ry=ry+romanDateMonth(s);
+            }
+            else
+            {
+                ry=ry+romanYear(Integer.toString(50-d))+romanYear("50");
+            }
+        }
         else if(d>=50 && d<100)
         {
-            int d1=d/50;
-            for(i=1;i<=d1;i++)
-                ry=ry+"L";
-            d1=d%50;
-            ry=ry+romanYear(Integer.toString(d1));
+            if(d<90)
+            {
+                int d1=d/50;
+                for(i=1;i<=d1;i++)
+                    ry=ry+"L";
+                d1=d%50;
+                ry=ry+romanYear(Integer.toString(d1));
+            }
+            else
+            {
+                ry=ry+romanYear("10")+romanYear("100");
+            }
         }
         else if(d>=100 && d<500)
         {
-            int d1=d/100;
-            for(i=1;i<=d1;i++)
-                ry=ry+"C";
-            d1=d%100;
-            ry=ry+romanYear(Integer.toString(d1));
+            
+                int d1=d/100;
+                for(i=1;i<=d1;i++)
+                    ry=ry+"C";
+                    d1=d%100;
+                    ry=ry+romanYear(Integer.toString(d1));
+            
         }
         else if(d>=500 && d<1000)
         {
-            int d1=d/500;
-            for(i=1;i<=d1;i++)
-                ry=ry+"D";
-            d1=d%500;
-            ry=ry+romanYear(Integer.toString(d1));
+            if(d<900)
+            {
+                int d1=d/500;
+                for(i=1;i<=d1;i++)
+                    ry=ry+"D";
+                d1=d%500;
+                ry=ry+romanYear(Integer.toString(d1));
+            }
+            else
+            {
+                ry=ry+romanYear("100")+romanYear("1000")+romanYear(Integer.toString(d-900));
+            }
         }
         else
         {
